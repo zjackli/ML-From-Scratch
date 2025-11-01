ff3b04a6eb15481a32751f2d3f2cdd9bf1187064
jackl07 <zonglin.li@bhspecialty.com>
2025-11-01 13:57:30 -0700

    note: polynomial regression study

--- mlfromscratch/utils/data_manipulation.py
+++ mlfromscratch/utils/data_manipulation.py
@@ -6,7 +6,7 @@ import sys
 
 
 def shuffle_data(X, y, seed=None):
-    """ Random shuffle of the samples in X and y """
+    """Random shuffle of the samples in X and y"""
     if seed:
         np.random.seed(seed)
     idx = np.arange(X.shape[0])
@@ -15,10 +15,10 @@ def shuffle_data(X, y, seed=None):
 
 
 def batch_iterator(X, y=None, batch_size=64):
-    """ Simple batch generator """
+    """Simple batch generator"""
     n_samples = X.shape[0]
     for i in np.arange(0, n_samples, batch_size):
-        begin, end = i, min(i+batch_size, n_samples)
+        begin, end = i, min(i + batch_size, n_samples)
         if y is not None:
             yield X[begin:end], y[begin:end]
         else:
@@ -26,8 +26,8 @@ def batch_iterator(X, y=None, batch_size=64):
 
 
 def divide_on_feature(X, feature_i, threshold):
-    """ Divide dataset based on if sample value on feature index is larger than
-        the given threshold """
+    """Divide dataset based on if sample value on feature index is larger than
+    the given threshold"""
     split_func = None
     if isinstance(threshold, int) or isinstance(threshold, float):
         split_func = lambda sample: sample[feature_i] >= threshold
@@ -44,22 +44,25 @@ def polynomial_features(X, degree):
     n_samples, n_features = np.shape(X)
 
     def index_combinations():
-        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
+        combs = [
+            combinations_with_replacement(range(n_features), i)
+            for i in range(0, degree + 1)
+        ]
         flat_combs = [item for sublist in combs for item in sublist]
         return flat_combs
-    
+
     combinations = index_combinations()
     n_output_features = len(combinations)
     X_new = np.empty((n_samples, n_output_features))
-    
-    for i, index_combs in enumerate(combinations):  
+
+    for i, index_combs in enumerate(combinations):
         X_new[:, i] = np.prod(X[:, index_combs], axis=1)
 
     return X_new
 
 
 def get_random_subsets(X, y, n_subsets, replacements=True):
-    """ Return random subsets (with replacements) of the data """
+    """Return random subsets (with replacements) of the data"""
     n_samples = np.shape(X)[0]
     # Concatenate x and y and do a random shuffle
     X_y = np.concatenate((X, y.reshape((1, len(y))).T), axis=1)
@@ -69,13 +72,12 @@ def get_random_subsets(X, y, n_subsets, replacements=True):
     # Uses 50% of training samples without replacements
     subsample_size = int(n_samples // 2)
     if replacements:
-        subsample_size = n_samples      # 100% with replacements
+        subsample_size = n_samples  # 100% with replacements
 
     for _ in range(n_subsets):
         idx = np.random.choice(
-            range(n_samples),
-            size=np.shape(range(subsample_size)),
-            replace=replacements)
+            range(n_samples), size=np.shape(range(subsample_size)), replace=replacements
+        )
         X = X_y[idx][:, :-1]
         y = X_y[idx][:, -1]
         subsets.append([X, y])
@@ -83,14 +85,14 @@ def get_random_subsets(X, y, n_subsets, replacements=True):
 
 
 def normalize(X, axis=-1, order=2):
-    """ Normalize the dataset X """
+    """Normalize the dataset X"""
     l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
     l2[l2 == 0] = 1
     return X / np.expand_dims(l2, axis)
 
 
 def standardize(X):
-    """ Standardize the dataset X """
+    """Standardize the dataset X"""
     X_std = X
     mean = X.mean(axis=0)
     std = X.std(axis=0)
@@ -102,7 +104,7 @@ def standardize(X):
 
 
 def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
-    """ Split the data into train and test sets """
+    """Split the data into train and test sets"""
     if shuffle:
         X, y = shuffle_data(X, y, seed)
     # Split the training data from test data in the ratio specified in
@@ -115,13 +117,13 @@ def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
 
 
 def k_fold_cross_validation_sets(X, y, k, shuffle=True):
-    """ Split the data into k sets of training / test data """
+    """Split the data into k sets of training / test data"""
     if shuffle:
         X, y = shuffle_data(X, y)
 
     n_samples = len(y)
     left_overs = {}
-    n_left_overs = (n_samples % k)
+    n_left_overs = n_samples % k
     if n_left_overs != 0:
         left_overs["X"] = X[-n_left_overs:]
         left_overs["y"] = y[-n_left_overs:]
@@ -133,8 +135,8 @@ def k_fold_cross_validation_sets(X, y, k, shuffle=True):
     sets = []
     for i in range(k):
         X_test, y_test = X_split[i], y_split[i]
-        X_train = np.concatenate(X_split[:i] + X_split[i + 1:], axis=0)
-        y_train = np.concatenate(y_split[:i] + y_split[i + 1:], axis=0)
+        X_train = np.concatenate(X_split[:i] + X_split[i + 1 :], axis=0)
+        y_train = np.concatenate(y_split[:i] + y_split[i + 1 :], axis=0)
         sets.append([X_train, X_test, y_train, y_test])
 
     # Add left over samples to last set as training samples
@@ -142,11 +144,12 @@ def k_fold_cross_validation_sets(X, y, k, shuffle=True):
         np.append(sets[-1][0], left_overs["X"], axis=0)
         np.append(sets[-1][2], left_overs["y"], axis=0)
 
-    return np.array(sets)
+    return sets
+    # return np.array(sets)
 
 
 def to_categorical(x, n_col=None):
-    """ One-hot encoding of nominal values """
+    """One-hot encoding of nominal values"""
     if not n_col:
         n_col = np.amax(x) + 1
     one_hot = np.zeros((x.shape[0], n_col))
@@ -155,12 +158,12 @@ def to_categorical(x, n_col=None):
 
 
 def to_nominal(x):
-    """ Conversion from one-hot encoding to nominal """
+    """Conversion from one-hot encoding to nominal"""
     return np.argmax(x, axis=1)
 
 
 def make_diagonal(x):
-    """ Converts a vector into an diagonal matrix """
+    """Converts a vector into an diagonal matrix"""
     m = np.zeros((len(x), len(x)))
     for i in range(len(m[0])):
         m[i, i] = x[i]
--- mlfromscratch/utils/data_operation.py
+++ mlfromscratch/utils/data_operation.py
@@ -5,7 +5,7 @@ import sys
 
 
 def calculate_entropy(y):
-    """ Calculate the entropy of label array y """
+    """Calculate the entropy of label array y"""
     log2 = lambda x: math.log(x) / math.log(2)
     unique_labels = np.unique(y)
     entropy = 0
@@ -17,28 +17,36 @@ def calculate_entropy(y):
 
 
 def mean_squared_error(y_true, y_pred):
-    """ Returns the mean squared error between y_true and y_pred """
+    """Returns the mean squared error between y_true and y_pred"""
     mse = np.mean(np.power(y_true - y_pred, 2))
     return mse
 
 
+def r_square(y_true, y_pred):
+    """Calculate R2"""
+    # calculate R² (coefficient of determination)
+    ss_res = np.sum((y_true - y_pred) ** 2)  # residual sum of squares
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)  # total sum of squares
+    return 1 - (ss_res / ss_tot)
+
+
 def calculate_variance(X):
-    """ Return the variance of the features in dataset X """
+    """Return the variance of the features in dataset X"""
     mean = np.ones(np.shape(X)) * X.mean(0)
     n_samples = np.shape(X)[0]
     variance = (1 / n_samples) * np.diag((X - mean).T.dot(X - mean))
-    
+
     return variance
 
 
 def calculate_std_dev(X):
-    """ Calculate the standard deviations of the features in dataset X """
+    """Calculate the standard deviations of the features in dataset X"""
     std_dev = np.sqrt(calculate_variance(X))
     return std_dev
 
 
 def euclidean_distance(x1, x2):
-    """ Calculates the l2 distance between two vectors """
+    """Calculates the l2 distance between two vectors"""
     distance = 0
     # Squared distance between each coordinate
     for i in range(len(x1)):
@@ -47,23 +55,25 @@ def euclidean_distance(x1, x2):
 
 
 def accuracy_score(y_true, y_pred):
-    """ Compare y_true to y_pred and return the accuracy """
+    """Compare y_true to y_pred and return the accuracy"""
     accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
     return accuracy
 
 
 def calculate_covariance_matrix(X, Y=None):
-    """ Calculate the covariance matrix for the dataset X """
+    """Calculate the covariance matrix for the dataset X"""
     if Y is None:
         Y = X
     n_samples = np.shape(X)[0]
-    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+    covariance_matrix = (1 / (n_samples - 1)) * (X - X.mean(axis=0)).T.dot(
+        Y - Y.mean(axis=0)
+    )
 
     return np.array(covariance_matrix, dtype=float)
- 
+
 
 def calculate_correlation_matrix(X, Y=None):
-    """ Calculate the correlation matrix for the dataset X """
+    """Calculate the correlation matrix for the dataset X"""
     if Y is None:
         Y = X
     n_samples = np.shape(X)[0]
